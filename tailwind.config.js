bfca9626e020960f3759d0e0787b64447aba581d
DavidIsDawit <daveabraham83@gmail.com>
2026-01-26 18:33:32 +0300

    updated tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,16 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
-      sans: "Roboto Mono",
-    },
+        // This replaces Roboto Mono and sets Source Sans 3 as the default
+        sans: ['"Source Sans 3"', 'sans-serif'],
+      },
 
     extend: {
       scale: {
         120: "1.2",
       },
       colors: {
-        primary: "#00A3E0",     
+        primary: "#04A9C1",     
         secondary: "#0B162C",
         tertiary :"#B6BFDA",
         primaryHover: "#0d1476ff", 
