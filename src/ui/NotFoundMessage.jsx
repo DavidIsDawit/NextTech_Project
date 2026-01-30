e6ad9a3425a837880f1d1d9015e2ccc31534a762
DavidIsDawit <daveabraham83@gmail.com>
2026-01-30 16:52:16 +0300

    change  on notfound sizeee

--- src/ui/NotFoundMessage.jsx
+++ src/ui/NotFoundMessage.jsx
@@ -6,7 +6,7 @@ export default function NotFoundMessage({ itemType = "Item", backPath }) {
   const navigate = useNavigate();
 
   return (
-    <div className="min-h-[50vh] bg-gray-50 flex items-center justify-center px-4 mt-20 mb-20">
+    <div className="min-h-[80vh] bg-gray-50 flex items-center justify-center px-4 mt-20 mb-20">
             
       <div className="text-center max-w-md">
         {/* Calm icon */}
